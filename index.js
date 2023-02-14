8309d0240a4c3d2f5b3d02f0b21dc31d0cddaf15
Ausha123 <aushasandeepa569@gmail.com>
2023-02-14 16:03:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,74 @@
+/*
+let name = 'Ausha'
+console.log(name);
+
+>>Cannot be a reserved keyword
+>>Should be meaningful
+>>Cannot start with a number (1name)
+>>Cannot contain a space or hypen(-)
+>>Are case-sensitive
+
+
+
+const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);  print error we cannot reasign a constant 
+*/
+
+/*Primitive Data Types
+let name = 'Ausha'; //String Literal
+let age = 20;//Number Literal
+let isApproved = false;//Boolean Literal
+let firstName = undefined;
+let selectedColor=null;
+*/
+
+
+
+
+/*Objects
+
+
+let person = {
+   name: 'Ausha',
+   age: 20
+};
+
+//Dot Notation
+person.name ='Sandeepa';
+
+//Bracket Notation
+person['name'] = 'Mary';
+
+console.log(person.name);
+
+*/
+
+// Arrays
+
+// let colors = ['red','Blue'];
+// console.log(colors);
+
+//Accsess Array Elemens
+
+// let colors = ['red','Blue'];
+// colors[2]='Green'
+// console.log(colors);
+
+
+
+function great(name, lastName){
+  console.log('Hello' + name  + '' + lastName);
+}
+
+great('Ausha','Sandeepa');
+
+function square(number){
+    number * number
+}
+
+console.log(square(2));
+
+
+
+
